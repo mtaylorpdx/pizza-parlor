15884b678c3569dfbe7497557b7856561cb9a2c1
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-31 11:00:16 -0800

    progress toward pizza size behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
-// Business Logic
+// Business Logic ------------
 
-function Pizza (size, topping, price) {
+function Pizza (size, toppings, price) {
   this.size = size;
   this.toppings = [];
   this.price = 0;
 }
 
+Pizza.prototype.pizzaSize = function() {
 
-// User Interface Logic
+}
+
+
+// User Interface Logic ------
+var pizza = new Pizza ('size', 'toppings', 'price')
+
+$(document).ready(function() {
+  $("form#size").submit(function(event) {
+    event.preventDefault();
+
+  });
+});
